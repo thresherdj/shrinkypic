137506efee92fa5017d5e7a8761444b001f4823c
thresherdj <sparkycbr@gmail.com>
2014-04-15 21:15:40 +0700

    Add Output check box to gui

--- lib/shrinkypic/icon/pyresources.py
+++ lib/shrinkypic/icon/pyresources.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Sun Feb 16 20:04:25 2014
+# Created: Tue Apr 15 21:11:37 2014
 #      by: The Resource Compiler for PySide (Qt v4.8.0)
 #
 # WARNING! All changes made in this file will be lost!
